f7bc47be7439e02c9fa72255dac536da2c4feda6
Sahan <ks.official.sahan@gmail.com>
2024-06-02 17:32:26 +0530

    Chart Integration

---
+++ src/ewision/sahan/event/MenuSelectedEvent.java
@@ -0,0 +1,10 @@
+package ewision.sahan.event;
+
+/**
+ *
+ * @author ksoff
+ */
+public interface MenuSelectedEvent {
+
+    public void selected(int index);
+}
---
+++ src/ewision/sahan/swing/ButtonCustom.java
@@ -0,0 +1,32 @@
+package ewision.sahan.swing;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author ksoff
+ */
+public class ButtonCustom extends JButton {
+    
+    public ButtonCustom() {
+        setContentAreaFilled(false);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+        setBackground(new Color(65, 152, 216));
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(10, 10, getWidth() - 20, getHeight() - 20, 20, 20);
+        super.paintComponent(g);
+    }
+    
+}
